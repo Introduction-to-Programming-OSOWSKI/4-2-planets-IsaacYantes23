7843c9048ac466ff0cbb3a8a3aafe6e6b77d3eb8
IsaacYantes23 <817296@mystma.org>
2021-02-17 16:09:26 +0000

    planets try 1'

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def planets(p):
+    planet=["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"]
+    for i in range(0,len(planet)):
+        if p == planet[i]:
+            return i
+       
+    return p + " is not a planet"
+    
+    
+
+print(planets("texas"))
