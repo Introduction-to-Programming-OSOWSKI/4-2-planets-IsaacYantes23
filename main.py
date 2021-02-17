4d64e103f85a7e0f41cd8b3994886f1f1cd8c7b8
IsaacYantes23 <817296@mystma.org>
2021-02-17 16:12:53 +0000

    planetstry2

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def planets(p):
     planet=["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"]
     for i in range(0,len(planet)):
         if p == planet[i]:
-            return i
+            return i + 1
        
     return p + " is not a planet"
     
     
 
-print(planets("texas"))
+print(planets("mercury"))
